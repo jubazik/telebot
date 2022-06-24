0b7e3318e7af4b10053fd63e759560aed74b1932
zizik <hitgas12@mail.ru>
2022-06-24 16:38:03 +0300

    Подключение бд

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,12 @@
+import app
 from telebot import TeleBot
-from api.config.config import TOKEN
+import sqlalchemy
+from api.config.config import TOKEN, Config
+
+
+app.config.from_object(Config)
+db = sqlalchemy(app)
+
+
 
 bot = TeleBot(TOKEN, parse_mode=None) # Подключение токена
--- api/function/functional.py
+++ api/function/functional.py
@@ -1,6 +1,7 @@
 from api import bot
 
-@bot.message_handler(commands=['start']) # Функция что б поздороваться с пользователем телебота
+
+@bot.message_handler(commands=['start'])  # Функция что б поздороваться с пользователем телебота
 def welcom(message):
     sri = open('welcom.webp', 'rb')
     bot.send_sticker(message.chat.id, sri)
@@ -9,6 +10,14 @@ def welcom(message):
                      "Ты уверен что хочешь сюда? \n я бот созданный чтобы быть подоподным кроликом", parse_mode='html')
 
 
-@bot.message_handler(content_types=['text']) #  Функция эхо
+@bot.message_handler(commands=['Кино'])
+def parsing(message):
+    pass
+
+
+
+
+
+@bot.message_handler(content_types=['text'])  # Функция эхо
 def echo(message):
     bot.send_message(message.chat.id, message.text)
